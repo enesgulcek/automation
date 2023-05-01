559b6713ba1757562edc3afea6bb48cd71fdac88
Enes Gülcek <115017534+enesgulcek@users.noreply.github.com>
2023-05-01 12:02:06 +0200

    Hash Generator for MD5 SHA1 SHA256 for Word Combination

--- md5hashmaker.py
+++ md5hashmaker.py
@@ -1,10 +1,19 @@
 import hashlib
 import itertools
 
-def hash_maker(word_list, num_letters=3, allow_repeats=False):
+def hash_maker(word_list, num_letters=3, allow_repeats=True, hash_algorithm='sha1'):
     """
-Diese Funktion nimmt eine Liste von Wörtern und generiert mit der MD5-Hash-Funktion einen Hash für jede mögliche Buchstabenkombination. Wenn num_letters angegeben wird, wird nur diese Anzahl von Buchstaben aus jedem Wort verwendet. Wenn allow_repeats True ist, können Buchstaben in den Kombinationen wiederholt werden.
+  Diese Funktion nimmt eine Liste von Wörtern und generiert einen Hash für jede mögliche Buchstabenkombination unter Verwendung des angegebenen Hash-Algorithmus. Wenn num_letters angegeben wird, wird nur diese Anzahl von Buchstaben aus jedem Wort verwendet. Wenn allow_repeats True ist, können Buchstaben in den Kombinationen wiederholt werden.
     """
+    if hash_algorithm == 'md5':
+        hasher = hashlib.md5
+    elif hash_algorithm == 'sha1':
+        hasher = hashlib.sha1
+    elif hash_algorithm == 'sha256':
+        hasher = hashlib.sha256
+    else:
+        raise ValueError("Invalid hash algorithm. Supported values are 'md5', 'sha1', and 'sha256'.")
+
     for word in word_list:
         if num_letters is None:
             letter_range = range(1, len(word)+1)
@@ -17,12 +26,14 @@ Diese Funktion nimmt eine Liste von Wörtern und generiert mit der MD5-Hash-Funk
                 combinations = itertools.combinations(word, r)
             for combination in combinations:
                 string = ''.join(combination)
-                hashed_string = hashlib.md5(string.encode()).hexdigest()
+                hashed_string = hasher(string.encode()).hexdigest()
                 print(f"{string}: {hashed_string}")
 
-# Example usage:
+# Beispielverwendung:
 word_list = ['abcde12345']
-hash_maker(word_list)  # Hash alle Buchstabenkombinationen jedes Wortes
-#hash_maker(word_list, num_letters=3)  # Hash nur 3-Buchstaben-Kombinationen jedes Wortes
-#hash_maker(word_list, allow_repeats=True)  # Hash alle Buchstabenkombinationen jedes Wortes mit Wiederholungen
-#hash_maker(word_list, num_letters=3, allow_repeats=True)  # Hash nur 3-Buchstaben-Kombinationen jedes Wortes mit Wiederholungen
+hash_maker(word_list) # Hash alle Buchstabenkombinationen jedes Wortes mit MD5
+hash_maker(word_list, num_letters=3) # Hash nur 3-Buchstaben-Kombinationen jedes Wortes mit MD5
+hash_maker(word_list, allow_repeats=True) # Hash alle Buchstabenkombinationen jedes Wortes mit Wiederholungen unter Verwendung von MD5
+hash_maker(word_list, num_letters=3, allow_repeats=True) # Hash nur 3-Buchstaben-Kombinationen jedes Wortes mit Wiederholungen unter Verwendung von MD5
+hash_maker(word_list, hash_algorithm='sha1') # Hash alle Buchstabenkombinationen jedes Wortes mit SHA1
+hash_maker(word_list, num_letters=3, hash_algorithm='sha256') # Hash nur 3-Buchstaben-Kombinationen jedes Wortes mit SHA256
